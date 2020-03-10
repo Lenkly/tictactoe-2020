eb9c998933757384594702fed86507f2a1656a7e
lenkly <lenk.lydia@gmail.com>
2020-03-10 11:39:42 +0100

    Passing data through props

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,26 +1,26 @@
-import React from "react";
-import Square from "./Square";
+import React from 'react';
+import Square from './Square';
 
 export default function Board() {
-  const status = "Next player: X";
+  const status = 'Next player: X';
 
   return (
     <div>
       <div className="status">{status}</div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="0" />
+        <Square value="1" />
+        <Square value="2" />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="3" />
+        <Square value="4" />
+        <Square value="5" />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="6" />
+        <Square value="7" />
+        <Square value="8" />
       </div>
     </div>
   );
